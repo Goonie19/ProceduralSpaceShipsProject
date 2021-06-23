9e9767565a0adccdd34b4ef693afdc38ea863bac
josioque <joseblazquez44@gmail.com>
2021-06-23 18:57:00 +0200

    Fondo en movimiento

---
+++ Assets/Scripts/BGScroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+    public float speed;
+    public float cutPoint;
+    public Vector3 startPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        if(transform.position.y < -cutPoint)
+        {
+            transform.position = startPosition;
+        }
+    }
+}
