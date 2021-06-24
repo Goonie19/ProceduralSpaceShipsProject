2fe121c90c075636c800281df50d9d1e9d507366
josioque <joseblazquez44@gmail.com>
2021-06-24 20:37:00 +0200

    changes
    he cambiado el fondo, ya hay menu de pausa
    He creado un prefab de estrella
    He hecho la UI para el juego

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,4 +14,14 @@ public class GameManager : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+    }
 }
---
+++ Assets/Scripts/SpawnerScripts/StartData/StartBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBehaviour : MonoBehaviour
+{
+    public float speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("SpaceShip"))
+        {
+            Debug.Log("Estrella cogida");
+            Destroy(gameObject);
+        }
+    }
+}
