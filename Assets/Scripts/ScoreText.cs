263ae4ce952d5cd844567dd530a9fc4e195e8119
josioque <joseblazquez44@gmail.com>
2021-07-24 17:59:12 +0200

    Script que actualiza las estrellas en la tienda

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreText : MonoBehaviour
+{
+    private TextMeshProUGUI scoreText;
+    private int storedScore;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+        UpdateScoreText();
+    }
+
+    public void UpdateScoreText()
+    {
+        storedScore = PlayerPrefs.GetInt("Stored score");
+
+        scoreText.text = "X  " + storedScore.ToString();
+    }
+
+    private void OnEnable()
+    {
+        UpdateScoreText();
+    }
+}
